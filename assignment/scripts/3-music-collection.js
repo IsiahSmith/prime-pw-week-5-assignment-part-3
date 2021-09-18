50e055d3c3d92acce8e2496a9ce924a50a3090e5
Isiah Smith <isiahthomassmith@gmail.com>
2021-09-18 12:18:17 -0500

    Added addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  collection.push(album);
+  return album;
+}
+
+console.log('Album Added to Collection:', addToCollection('The Melodic Blue', 'Baby Keem', 2021));
+console.log(collection);
