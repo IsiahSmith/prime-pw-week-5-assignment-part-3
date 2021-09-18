1b542715261723862f25e6e5b762db9b340fd0bf
Isiah Smith <isiahthomassmith@gmail.com>
2021-09-18 12:29:56 -0500

    Logged six albums to test function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,4 +12,9 @@ function addToCollection(title, artist, yearPublished) {
 }
 
 console.log('Album Added to Collection:', addToCollection('The Melodic Blue', 'Baby Keem', 2021));
+console.log('Album Added to Collection:', addToCollection('Certified Lover Boy', 'Drake', 2021));
+console.log('Album Added to Collection:', addToCollection('Eternal Atake', 'Lil Uzi Vert', 2020));
+console.log('Album Added to Collection:', addToCollection('ATLiens', 'Outkast', 1996));
+console.log('Album Added to Collection:', addToCollection('Legends Never Die', 'Juice WRLD', 2020));
+console.log('Album Added to Collection:', addToCollection('Luv Is Rage', 'Lil Uzi Vert', 2015));
 console.log(collection);
